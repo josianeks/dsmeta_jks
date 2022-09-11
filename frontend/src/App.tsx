974bc75be826ce15cb4f45f8e7c0dbcb1f30b65c
josianeks <josiks19@gmail.com>
2022-09-11 00:55:53 -0300

    creating component header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
     <>
-      <h1>Olá Mundo!</h1>
-      <NotificationButton />
-      <NotificationButton />
+      <Header/>
+      <NotificationButton/>
+      <NotificationButton/>
     </>
   )
 }
